8f4fbfa3b87491cf8e1d33d90e3e119c4119a8b3
TianMyy <mingyu_tian@outlook.com>
2021-06-18 10:58:52 +0800

    class design for Node to use for Red Black Tree

---
+++ Data-structures/Trees-advanced/Node.java
@@ -0,0 +1,17 @@
+package tree_advanced;
+
+public class Node<Key, Value> {
+	public Key key;
+	public Value value;
+	public Node left;
+	public Node right;
+	public boolean color;
+	
+	public Node(Key key, Value value, Node left, Node right, boolean color) {
+		this.key = key;
+		this.value = value;
+		this.left = left;
+		this.right = right;
+		this.color = color;
+	}
+}
