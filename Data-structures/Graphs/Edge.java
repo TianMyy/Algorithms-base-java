d2e0cf925e9297ddbd4c2b2b353575eb772a0176
TianMyy <mingyu_tian@outlook.com>
2021-06-21 10:57:33 +0800

    class design for edge and Weighted No-directed Graph structure

---
+++ Data-structures/Graphs/Edge.java
@@ -0,0 +1,46 @@
+package graph;
+
+public class Edge implements Comparable<Edge> {
+	private final int v; // point one
+	private final int w; // point two
+	private final double weight; // weighted value of current edge
+	
+	public Edge(int v, int w, double weight) {
+		this.v = v;
+		this.w = w;
+		this.weight = weight;
+	}
+	
+	public double weight() {
+		return weight;
+	}
+	
+	// get a point of edge
+	public int either() {
+		return v;
+	}
+	
+	// get another point of edge except point vertex
+	public int other(int vertex) {
+		if(vertex == v) {
+			return w;
+		}else {
+			return v;
+		}
+	}
+	
+	// compare weight between current edge and edge that
+	// if current is higher, return 1, if the same return 0, else return -1
+	@Override
+	public int compareTo(Edge that) {
+		int comp;
+		if(this.weight() > that.weight) {
+			comp = 1;
+		}else if(this.weight() == that.weight) {
+			comp = 0;
+		}else {
+			comp = -1;
+		}
+		return comp;
+	}
+}
---
+++ Data-structures/Graphs/EdgeWeightedGraph.java
@@ -0,0 +1,65 @@
+package graph;
+
+import queue.Queue;
+
+public class EdgeWeightedGraph {
+	// records number of points
+	private final int V;
+	// number of edges
+	private int E;
+	// adjacency list
+	private Queue<Edge>[] adj;
+	
+	public EdgeWeightedGraph(int V) {
+		this.V = V;
+		this.E = 0;
+		this.adj = new Queue[V];
+		// initiate empty queue in adj
+		for(int i = 0; i < adj.length; i++) {
+			adj[i] = new Queue<Edge>();
+		}
+	}
+	
+	// get number of points in graph
+	public int V() {
+		return V;
+	}
+	
+	// get number of edges in graph
+	public int E() {
+		return E;
+	}
+	
+	// add an edge e into weighted no-directed graph
+	public void addEdge(Edge e) {
+		// get point on
+		int v = e.either();
+		// get point two
+		int w = e.other(v);
+		// cause is no-directed graph, so edge e need to appear in two points' adajacency list
+		adj[v].enqueue(e);
+		adj[w].enqueue(e);
+		
+		E++;
+	}
+	
+	// get all related edges of point v
+	public Queue<Edge> adj(int v) {
+		return adj[v];
+	}
+	
+	// get all edges of weighted no-directed graph
+	public Queue edges() {
+		// create a queue to store all edges
+		Queue<Edge> edges = new Queue<>();
+		// loop points get adj of each point
+		for(int i = 0; i < V; i++) {
+			for(Edge e : adj(i)) {
+				if(e.other(i) < i) {
+					edges.enqueue(e);
+				}
+			}
+		}
+		return edges;
+	}
+}
