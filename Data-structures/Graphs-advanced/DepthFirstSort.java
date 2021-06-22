bb86ccebecd714e7b28bd5ca190b7dd192437622
TianMyy <mingyu_tian@outlook.com>
2021-06-22 09:23:28 +0800

    points sort based on depth first search of graph

---
+++ Data-structures/Graphs-advanced/DepthFirstSort.java
@@ -0,0 +1,37 @@
+package graph_advanced;
+
+import stack.Stack;
+
+public class DepthFirstSort {
+	private boolean[] marked;
+	// use stack to store points
+	private Stack<Integer> reversePost;
+	
+	public DepthFirstSort(Digraph G) {
+		this.marked = new boolean[G.V()];
+		this.reversePost = new Stack<Integer>();
+		for(int i = 0; i < G.V(); i++) {
+			// if current point didn't searched, then search it
+			if(!marked[i]) {
+				dfs(G, i);
+			}
+		}
+	}
+	
+	// based on depth first search, generate sequential list of points 
+	private void dfs(Digraph G, int v) {
+		marked[v] = true;
+		// for each point v's adjacency list, get each point w
+		for(Integer w : G.adj(v)) {
+			if(!marked[w]) {
+				dfs(G, w);
+			}
+		}
+		reversePost.push(v);
+	}
+	
+	// get sequential list of points
+	public Stack reversePost() {
+		return reversePost;
+	}
+}
